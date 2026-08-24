5cbb46839d10078b16e8366f23a858fa0f17c587
ymeda <yogeswarreddy.meda@vconnectech.in>
2026-08-24 10:35:03 +0530

    updated the footer address

--- frontend-react/src/components/Footer/Footer.jsx
+++ frontend-react/src/components/Footer/Footer.jsx
@@ -189,7 +189,7 @@ export default function Footer({ setCurrentPage }) {
               </li>
               <li>
                 <svg fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path strokeLinecap="round" strokeLinejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                <span>VConnectTech Systems<br />4th floor, Plot No:6, Sector-3, HUDA Techno Enclave, Madhapur, Hyderabad, Telangana, India - 500081</span>
+                <span>VConnecTech Systems<br />4th floor, Plot No:6, Sector-3, HUDA Techno Enclave, Madhapur, Hyderabad, Telangana, India - 500081</span>
               </li>
             </ul>
             <div className="map-bg"></div>
